c4c21aa5ee5ffb11987e9232130fe809cc4d8178
Georgii Parla <georg3.gur@gmail.com>
2024-02-06 18:55:46 +0100

    fix 2

--- services.js
+++ services.js
@@ -1,12 +1,12 @@
 const http = require("https")
 
 function SendMessageWhatsApp(data) {
-    var TOKEN = "EAAM9570qzjUBOxTrxnEBwkrzJpEAHXXW88SI9mSdAtuOJuILFY1NJLHHDcDg24xyCWNMbm9LWra9QYuvfAbPTZCkuuklhXG3EaesBLuUCRGYEm7NN3mJ8j1e7sjtMvWUirm3zgvUHvbn2Gske5qL4ZBXcEvh2XuulVekMksLa3QuaJmytvF4JsRdelD21UlPL5rsNZAg9b2Jqas4ZAAZD"
+    var TOKEN = "EAAxmAZC3npGQBO6sY8DsZBFTH1nINWRyD0HRM0bZCapZBTNsatXOBLSK3mhhVClVtmBgXeLVxsz8GRxCMI9y303JvnKq3I0SJQxrv6VP5sYNPSqr8HZB5e3bf6NLZB8EevvGNHvYhQxAZCzoOhyko0rNmZB5Ie3bbQZB1CKkxbVrsqyQQE07kQEYWA6YXoQGxNXsk"
 
     // options to send requests
     const options = {
         host: "graph.facebook.com",
-        path: "/v19.0/176435792210714/messages",
+        path: "/v18.0/172669552602437/messages",
         method: "POST",
         body: data,
         headers: {
